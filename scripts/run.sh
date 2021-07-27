0970e7a271a090e1545236a6ea840f4fc84bb8ce
Ekrem Parlak <ekremparlak@outlook.com>
2021-07-27 22:46:02 +0200

    add port variable

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
-PORT=$(echo $RANDOM)
-echo reverse_proxy $SUBFOLDER http://127.0.0.1:$PORT >> /Caddyfile
-goaccess  --daemonize $SUBFOLDER -o /html$SUBFOLDER.html --log-format=$LOG_FORMAT --real-time-html --addr=0.0.0.0 --port=$PORT --ws-url=ws://$SERVER:80$SUBFOLDER
+WS_PORT=$(echo $RANDOM)
+echo reverse_proxy $SUBFOLDER http://127.0.0.1:$WS_PORT >> /Caddyfile
+goaccess  --daemonize $SUBFOLDER -o /html$SUBFOLDER.html --log-format=$LOG_FORMAT --real-time-html --addr=0.0.0.0 --port=$WS_PORT --ws-url=ws://$SERVER:$PORT$SUBFOLDER
