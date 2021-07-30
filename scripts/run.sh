c214609280718c3db1fce0594f9476186cce5f66
Ekrem Parlak <ekremparlak@outlook.com>
2021-07-30 15:47:05 +0200

    add anonymize option

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh 
 WS_PORT=$(echo $RANDOM)
+if [ "$ANONYMIZE" == "true" ]; then ANONYMIZE_IP="--anonymize-ip" ; fi
 echo reverse_proxy $SUBFOLDER http://127.0.0.1:$WS_PORT >> /Caddyfile
-goaccess $SUBFOLDER -o /html$SUBFOLDER.html --daemonize --log-format=$LOG_FORMAT --real-time-html --addr=0.0.0.0 --port=$WS_PORT --ws-url=ws://'" + location.host + "'$SUBFOLDER
+goaccess $SUBFOLDER -o /html$SUBFOLDER.html --daemonize  $ANONYMIZE_IP --log-format=$LOG_FORMAT --real-time-html --addr=0.0.0.0 --port=$WS_PORT --ws-url=ws://'" + location.host + "'$SUBFOLDER
--- scripts/run_static.sh
+++ scripts/run_static.sh
@@ -1,2 +1,3 @@
 #!/bin/sh 
-goaccess $SUBFOLDER -o /html$SUBFOLDER.html --log-format=$LOG_FORMAT
+if [ "$ANONYMIZE" == "true" ]; then ANONYMIZE_IP="--anonymize-ip" ; fi
+goaccess $SUBFOLDER -o /html$SUBFOLDER.html --log-format=$LOG_FORMAT $ANONYMIZE_IP
